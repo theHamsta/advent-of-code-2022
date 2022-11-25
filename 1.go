e5fa48fad38b40d51d696ca7ea08a2251c69ae52
Stephan Seitz <stephan.seitz@fau.de>
2022-11-26 00:23:28 +0100

    Initial commit

---
+++ 1.go
@@ -0,0 +1,17 @@
+//
+// 1.go
+// Copyright (C) 2022 Stephan Seitz <stephan.seitz@fau.de>
+//
+// Distributed under terms of the GPLv3 license.
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	if 1 == 1 {
+		fmt.Println("No! Not again")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module theHamsta/advent-of-code-2022
+
+go 1.18
