6c058bbd06506fb716c465311032adb4a77036a0
Stephan Seitz <stephan.seitz@fau.de>
2022-11-28 22:38:57 +0100

    Read file

--- src/day_01.go
+++ src/day_01.go
@@ -1,5 +1,16 @@
 package advent
 
+import (
+	"fmt"
+	"os"
+)
+
 func Day01() {
+	input, err := os.ReadFile("input/day01.txt")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(1)
+	}
+	fmt.Print(input)
 
 }
