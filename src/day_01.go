bea92326b6150ab8275d355e952aa3c2b484a406
Stephan Seitz <stephan.seitz@fau.de>
2022-12-01 06:17:37 +0100

    Go: solve day1

--- src/day_01.go
+++ src/day_01.go
@@ -3,7 +3,21 @@ package advent
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 )
+
+type Point struct {
+	x, y   int
+	dx, dy int
+}
+
+func toInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
 
 func Day01() {
 	input, err := os.ReadFile("input/day01.txt")
@@ -11,6 +25,27 @@ func Day01() {
 		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
-	fmt.Print(input)
 
+	elves := strings.Split(string(input), "\n\n")
+	regex := regexp.MustCompile(`(\d+)`)
+	argmax := -1
+	max := 0
+	calories := make([]int, len(elves))
+	for i, elf := range elves {
+		parsed := regex.FindAllStringSubmatch(elf, -1)
+		sum := 0
+		for _, c := range parsed {
+			sum += toInt(c[1])
+		}
+		calories[i] = sum
+		if calories[i] > max {
+			max = calories[i]
+			argmax = i + 1
+		}
+	}
+
+	fmt.Println("part 1:", max, "(argmax: ", argmax, ")")
+
+	sort.Ints(calories)
+	fmt.Println("part 2:", calories[len(calories)-1]+calories[len(calories)-2]+calories[len(calories)-3])
 }
