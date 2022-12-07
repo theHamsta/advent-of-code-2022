04242da83b7de7055b4b15c86ae97f292971e55e
Stephan Seitz <stephan.seitz@fau.de>
2022-12-07 08:09:37 +0100

    Solve day 07

---
+++ rust/day07/src/main.rs
@@ -0,0 +1,185 @@
+use std::{cell::RefCell, rc::Rc, sync::Mutex};
+
+use regex::Regex;
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+enum Instruction {
+    Cd(String),
+    Ls,
+}
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+enum Output {
+    File { file: String, size: usize },
+    Dir(String),
+}
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+struct Size {
+    file: String,
+    size: usize,
+}
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+struct Commmand {
+    instruction: Instruction,
+    output: Vec<Output>,
+}
+
+#[derive(Debug, Clone)]
+struct Tree {
+    name: String,
+    output: Option<Vec<Output>>,
+    children: Vec<Rc<Mutex<Tree>>>,
+    parent: Option<Rc<Mutex<Tree>>>,
+    size: usize
+}
+
+fn part1(tree: &mut Tree) -> (usize, usize) {
+    let mut total = 0;
+    let mut total_score = 0;
+    for child in &tree.children {
+        let (child_size, child_score) = part1(&mut child.lock().unwrap());
+        total += child_size;
+        total_score += child_score;
+    }
+    if let Some(output) = &tree.output {
+        for file in output {
+            if let Output::File { size, .. } = file {
+                total += size;
+            }
+        }
+    }
+    if total <= 100000 {
+        total_score += total;
+    }
+    tree.size = total;
+    (total, total_score)
+}
+
+fn part2(tree: &mut Tree, needed: usize) -> usize {
+    let mut min = usize::MAX;
+    if tree.size > needed {
+        min = tree.size;
+    }
+    for child in &tree.children {
+        let child_min = part2(&mut child.lock().unwrap(), needed);
+        if child_min < min {
+            min = child_min;
+        }
+    }
+    min
+}
+
+fn main() {
+    let input = include_str!("../../../input/day07.txt");
+
+    let mut commands = Vec::new();
+
+    let mut instruction = None;
+    let mut output = Vec::new();
+
+    for line in input.lines().filter(|line| !line.is_empty()) {
+        if line.starts_with("$") && instruction.is_some() {
+            commands.push(Commmand {
+                instruction: instruction.clone().unwrap(),
+                output: output.clone(),
+            });
+            output.clear();
+            instruction = None;
+        }
+
+        let cd_regex = Regex::new(r"\$ cd (.*)").unwrap();
+
+        if line == "$ ls" {
+            instruction = Some(Instruction::Ls);
+        } else if let Some(m) = cd_regex.captures_iter(line).next() {
+            instruction = Some(Instruction::Cd(m[1].to_string()));
+        } else {
+            let mut split = line.split_whitespace();
+            let size = split.next().unwrap();
+            let file = split.next().unwrap().to_owned();
+            let size_int: Option<usize> = size.parse().ok();
+            if let Some(size_int) = size_int {
+                output.push(Output::File {
+                    file,
+                    size: size_int,
+                });
+            } else {
+                output.push(Output::Dir(file));
+            }
+        }
+    }
+    if instruction.is_some() {
+        commands.push(Commmand {
+            instruction: instruction.clone().unwrap(),
+            output: output.clone(),
+        });
+        output.clear();
+    }
+
+    let tree = Rc::new(Mutex::new(Tree {
+        name: "/".to_string(),
+        output: Some(commands[1].output.clone()),
+        children: Vec::new(),
+        parent: None,
+        size: 0
+    }));
+
+    let mut cwd = Rc::clone(&tree);
+
+    for cmd in commands[1..].iter() {
+        match &cmd.instruction {
+            Instruction::Ls => {
+                cwd.lock().unwrap().children.clear();
+                for o in cmd.output.iter() {
+                    if let Output::Dir(name) = o {
+                        let child = Tree {
+                            name: name.to_string(),
+                            output: None,
+                            children: Vec::new(),
+                            parent: Some(Rc::clone(&cwd)),
+                            size: 0
+                        };
+                        cwd.lock()
+                            .unwrap()
+                            .children
+                            .push(Rc::new(Mutex::new(child)));
+                    }
+                }
+                cwd.lock().unwrap().output = Some(cmd.output.clone());
+            }
+            Instruction::Cd(path) => {
+                if path == ".." {
+                    let next;
+                    {
+                        let lock = cwd.lock().unwrap();
+                        next = Rc::clone(lock.parent.as_ref().unwrap());
+                    }
+                    cwd = next;
+                } else {
+                    let child;
+                    {
+                        let mut lock = cwd.lock().unwrap();
+                        child = Rc::clone(
+                            lock.children
+                                .iter_mut()
+                                .find(|c| c.lock().unwrap().name == *path)
+                                .unwrap(),
+                        );
+                    }
+                    cwd = child;
+                }
+            }
+        }
+    }
+
+
+    let part1 = part1(&mut tree.lock().unwrap());
+    dbg!(&part1);
+    let available = 70000000 - part1.0;
+    let needed = 30000000 - available;
+
+    let part2 = part2(&mut tree.lock().unwrap(), needed);
+    dbg!(&part2);
+}
