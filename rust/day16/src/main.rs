5b42a064adbe8f169cb94773bba1f7eb3ae1e57a
Stephan Seitz <stephan.seitz@fau.de>
2022-12-17 02:29:06 +0100

    Upload current solution

--- rust/day16/src/main.rs
+++ rust/day16/src/main.rs
@@ -1,5 +1,10 @@
-use std::collections::HashMap;
+use std::{
+    cell::RefCell,
+    collections::{HashMap, HashSet, VecDeque},
+    rc::Rc,
+};
 
+use petgraph::{dot::Dot, visit::EdgeRef, Graph};
 use regex::Regex;
 
 #[derive(Debug, Clone)]
@@ -9,67 +14,19 @@ struct Valve {
     neighbors: Vec<String>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-enum Action {
-    Open(String),
-    Move(String),
-}
+//#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+//enum Action {
+//Open(String),
+//Move(String),
+//}
 
 //type RtnType = (Vec<Action>, i64);
-type RtnType = i64;
+type RtnType = (Vec<u32>, u64);
 
-fn release_pipes<'state>(
-    graph: &HashMap<String, Valve>,
-    current: &str,
-    minutes: i64,
-    open_state: &'state mut Vec<bool>,
-    cache: &'state mut HashMap<(String, Vec<bool>, i64), RtnType>,
-) -> RtnType {
-    if minutes == 0 {
-        //return (Vec::new(), 0);
-        return  0;
-    }
-    if let Some(rtn) = cache.get(&(current.to_owned(), open_state.to_vec(), minutes)) {
-        return rtn.clone();
-    }
-    let freshly_released: i64 = graph
-        .iter()
-        .zip(open_state.iter())
-        .filter(|(_, open)| **open)
-        .map(|((_, v), _)| v.flow_rate)
-        .sum();
-    let cur_idx = graph.iter().position(|(_, n)| n.valve == current).unwrap();
-    let before_value = open_state[cur_idx];
-    open_state[cur_idx] = true;
-    let mut stay_here = release_pipes(graph, current, minutes - 1, open_state, cache);
-    open_state[cur_idx] = before_value;
-
-    let mut rtn = graph
-        .get(current)
-        .unwrap()
-        .clone()
-        .neighbors
-        .iter()
-        .map(|n| {
-            let rtn = release_pipes(graph, &n.clone(), minutes - 1, open_state, cache);
-            //rtn.0.push(Action::Move(n.to_owned()));
-            rtn
-        })
-        //.max_by_key(|(_, max)| *max)
-        .max()
-        .unwrap();
-    if stay_here > rtn {
-        //stay_here.0.push(Action::Open(current.to_owned()));
-        rtn = stay_here;
-    }
-
-    //rtn = (rtn.0, rtn.1 + freshly_released);
-    rtn  += freshly_released;
-    cache.insert(
-        (current.to_owned(), open_state.to_vec(), minutes),
-        rtn.clone(),
-    );
-    rtn
+#[derive(Debug, Clone)]
+struct Node {
+    _name: String,
+    flow_rate: u64,
 }
 
 fn main() {
@@ -95,9 +52,144 @@ fn main() {
         .collect();
 
     let graph: HashMap<_, Valve> = input.drain(..).map(|v| (v.valve.clone(), v)).collect();
-    let mut open_state = vec![false; graph.len()];
-    let mut cache = HashMap::new();
 
-    let part1 = release_pipes(&graph, "AA", 30, &mut open_state, &mut cache);
-    dbg!(&part1);
+    let mut petgraph = Graph::<Node, u64>::new();
+
+    let node_list: HashMap<_, _> = graph
+        .iter()
+        .map(|(k, node)| {
+            (
+                k.clone(),
+                petgraph.add_node(Node {
+                    _name: node.valve.to_owned(),
+                    flow_rate: node.flow_rate as u64,
+                }),
+            )
+        })
+        .collect();
+
+    for (my_name, node) in graph.iter() {
+        let my_idx = node_list[my_name];
+        for n in node.neighbors.iter() {
+            petgraph.add_edge(my_idx, node_list[n], 1);
+        }
+    }
+
+    let aa_idx = node_list["AA"];
+    let mut to_add = Vec::new();
+    for n in petgraph
+        .node_indices()
+        .filter(|idx| petgraph.node_weight(*idx).unwrap().flow_rate > 0 || node_list["AA"] == *idx)
+    {
+        let mut visited = HashSet::new();
+        let mut to_visit = VecDeque::new();
+        to_visit.push_back((n, 0));
+        visited.insert(n);
+        while let Some((between, hops)) = to_visit.pop_front() {
+            visited.insert(between);
+            for neighbor in petgraph.neighbors(between) {
+                if !visited.contains(&neighbor) {
+                    if petgraph.node_weight(neighbor).unwrap().flow_rate == 0 {
+                        to_visit.push_back((neighbor, hops + 1));
+                    } else {
+                        to_add.push((n, neighbor, hops + 1));
+                    }
+                }
+            }
+        }
+    }
+    for (a, b, w) in to_add {
+        petgraph.update_edge(a, b, w);
+    }
+
+    petgraph.retain_edges(|graph, e| {
+        let (a, b) = graph.edge_endpoints(e).unwrap();
+        (graph.node_weight(a).unwrap().flow_rate > 0 && graph.node_weight(b).unwrap().flow_rate > 0)
+            || a == aa_idx
+    });
+    println!("{:?}", Dot::new(&petgraph));
+
+    let mut open_state = vec![false; petgraph.raw_nodes().len()];
+    let visited_state = vec![false; petgraph.raw_nodes().len()];
+    let mut cache = HashMap::new();
+    let mut part1 = release_pipes_graph(
+        &petgraph,
+        node_list["AA"].index() as u32,
+        30,
+        &mut open_state,
+        &Rc::new(RefCell::new(visited_state)),
+        &mut cache,
+    );
+
+    dbg!(&part1.1);
+
+    while let Some(item) = part1.0.pop() {
+        let name = node_list
+            .iter()
+            .find(|(_, idx)| item == idx.index() as u32)
+            .unwrap()
+            .0;
+        println!("{name}");
+    }
+}
+
+fn release_pipes_graph(
+    graph: &Graph<Node, u64>,
+    current: u32,
+    minutes: i64,
+    open_state: &mut Vec<bool>,
+    visited: &Rc<RefCell<Vec<bool>>>,
+    cache: &mut HashMap<(u32, Vec<bool>, i64), RtnType>,
+) -> RtnType {
+    if minutes <= 0 {
+        return (Vec::new(), 0);
+    }
+    if let Some(rtn) = cache.get(&(current, open_state.to_vec(), minutes)) {
+        return rtn.clone();
+    }
+    visited.borrow_mut()[current as usize] = true;
+    let freshly_released: u64 = graph
+        .raw_nodes()
+        .iter()
+        .enumerate()
+        .filter(|(idx, _)| open_state[*idx])
+        .map(|(_, n)| n.weight.flow_rate)
+        .sum();
+    let before_value = open_state[current as usize];
+    open_state[current as usize] = true;
+    let mut stay_here =
+        release_pipes_graph(graph, current, minutes - 1, open_state, visited, cache);
+    stay_here.0.push(current);
+    stay_here.1 += freshly_released;
+    open_state[current as usize] = before_value;
+    let mut rtn = graph
+        .edges(current.into())
+        //.filter(|e| !visited.borrow()[e.target().index()])
+        .map(|e| {
+            let mut rtn = release_pipes_graph(
+                graph,
+                e.target().index() as u32,
+                minutes - *e.weight() as i64,
+                open_state,
+                visited,
+                cache,
+            );
+            rtn.0.push(current);
+            rtn.1 += (minutes - *e.weight() as i64).clamp(0, *e.weight() as i64) as u64
+                * freshly_released;
+            rtn
+        })
+        .max_by_key(|k| k.1)
+        .unwrap_or_else(|| (Vec::new(), 0));
+    if stay_here.1 > rtn.1 {
+        //stay_here.0.push(Action::Open(current.to_owned()));
+        rtn = stay_here;
+    }
+
+    cache.insert(
+        (current.to_owned(), open_state.to_vec(), minutes),
+        rtn.clone(),
+    );
+    visited.borrow_mut()[current as usize] = false;
+    rtn
 }
